c0df2fc79f9f75a459fcf373a8fef13fd02694c1
Sateesh Kumar D <sateeshkumar.d@gmail.com>
2019-07-28 14:20:59 +0530

    Logging properties have been added.
    Loggers have been added to the controller and service.

--- employee-service/src/main/java/com/example/emp/controller/EmployeeRestController.java
+++ employee-service/src/main/java/com/example/emp/controller/EmployeeRestController.java
@@ -14,45 +14,65 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.emp.entity.Employee;
 import com.example.emp.service.EmployeeService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class EmployeeRestController {
 
 	@Autowired
 	private EmployeeService employeeService;
 
-	public void setEmployeeService(EmployeeService employeeService) {
-		this.employeeService = employeeService;
-	}
-
 	@GetMapping("/api/employees")
 	public List<Employee> getEmployees() {
-		return employeeService.retrieveEmployees();
+		log.trace("getEmployees() - start");
+
+		List<Employee> returnList = employeeService.retrieveEmployees();
+		
+		log.trace("getEmployees() - end");
+		return returnList;
 	}
 
 	@GetMapping("/api/employees/{employeeId}")
 	public Employee getEmployee(@PathVariable(name = "employeeId") Long employeeId) {
-		return employeeService.getEmployee(employeeId);
+		log.trace("getEmployee(Long) - start");
+
+		Employee returnEmployee = employeeService.getEmployee(employeeId);
+		
+		log.trace("getEmployee(Long) - end");
+		return returnEmployee;
 	}
 
 	@PostMapping("/api/employees")
 	public void saveEmployee(Employee employee) {
+		log.trace("saveEmployee(Employee) - start");
+
 		employeeService.saveEmployee(employee);
-		System.out.println("Employee Saved Successfully");
+		log.info("saveEmployee(Employee) - {}", "Employee Saved Successfully");
+
+		log.trace("saveEmployee(Employee) - end");
 	}
 
 	@DeleteMapping("/api/employees/{employeeId}")
 	public void deleteEmployee(@PathVariable(name = "employeeId") Long employeeId) {
+		log.trace("deleteEmployee(Long) - start");
+
 		employeeService.deleteEmployee(employeeId);
-		System.out.println("Employee Deleted Successfully");
+		log.info("deleteEmployee(Long) - {}", "Employee Deleted Successfully");
+
+		log.trace("deleteEmployee(Long) - end");
 	}
 
 	@PutMapping("/api/employees/{employeeId}")
 	public void updateEmployee(@RequestBody Employee employee, @PathVariable(name = "employeeId") Long employeeId) {
+		log.trace("updateEmployee(Employee, Long) - start");
+
 		Employee emp = employeeService.getEmployee(employeeId);
 		if (emp != null) {
 			employeeService.updateEmployee(employee);
 		}
 
+		log.trace("updateEmployee(Employee, Long) - end");
 	}
 
 }
--- employee-service/src/main/java/com/example/emp/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/example/emp/service/impl/EmployeeServiceImpl.java
@@ -10,35 +10,58 @@ import com.example.emp.entity.Employee;
 import com.example.emp.repo.EmployeeRepository;
 import com.example.emp.service.EmployeeService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
-	public void setEmployeeRepository(EmployeeRepository employeeRepository) {
-		this.employeeRepository = employeeRepository;
-	}
-
 	public List<Employee> retrieveEmployees() {
-		List<Employee> employees = employeeRepository.findAll();
-		return employees;
+		log.trace("retrieveEmployees() - start");
+
+		List<Employee> returnList = employeeRepository.findAll();
+		log.trace("retrieveEmployees() - end");
+		return returnList;
 	}
 
 	public Employee getEmployee(Long employeeId) {
+		log.trace("getEmployee(Long) - start");
+
+		Employee employee = null;
 		Optional<Employee> optEmp = employeeRepository.findById(employeeId);
-		return optEmp.get();
+		
+		if (optEmp.isPresent()) {
+			employee = optEmp.get();
+		}
+		
+		log.trace("getEmployee(Long) - end");
+		return employee;
 	}
 
 	public void saveEmployee(Employee employee) {
+		log.trace("saveEmployee(Employee) - start");
+
 		employeeRepository.save(employee);
+
+		log.trace("saveEmployee(Employee) - end");
 	}
 
 	public void deleteEmployee(Long employeeId) {
+		log.trace("deleteEmployee(Long) - start");
+
 		employeeRepository.deleteById(employeeId);
+
+		log.trace("deleteEmployee(Long) - end");
 	}
 
 	public void updateEmployee(Employee employee) {
+		log.trace("updateEmployee(Employee) - start");
+
 		employeeRepository.save(employee);
+
+		log.trace("updateEmployee(Employee) - end");
 	}
 }
